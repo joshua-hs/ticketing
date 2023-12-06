b85d0a8b5c2e3f535d713426f7af9b99e502b4b9
joshua-hs <joshua.hs.harris@gmail.com>
2023-12-06 15:13:21 +0000

    test

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -48,7 +48,7 @@ const start = async () => {
     console.error(err);
   }
   app.listen(3000, () => {
-    console.log("Listening on port 3000 with abstracted app yo!!");
+    console.log("Listening on port 3000 with abstracted app yo!");
   });
 };
 
